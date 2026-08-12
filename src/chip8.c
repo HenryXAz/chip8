a40e9452003ad8066d5b59019038eca469e19f5d
HenryXiAz <henry.xitimul@micoopebienestar.com.gt>
2026-08-12 17:20:33 -0600

    Add DecodedOpcode struct, method to initialize DecodedOpcode and method to fetch opcode in memory and set in variable

--- src/chip8.c
+++ src/chip8.c
@@ -24,3 +24,29 @@ bool chip8_load_program(Chip8 *chip8, const uint8_t *program, size_t size) {
 
     return true;
 }
+
+static bool chip8_fetch_opcode(const Chip8 *chip8, uint16_t *opcode)  {
+    if (chip8->pc >= CHIP8_MEMORY_SIZE - 1) {
+        return false;
+    }
+
+    uint8_t high = chip8->memory[chip8->pc];
+    uint8_t low = chip8->memory[chip8->pc + 1];
+
+    *opcode = (high << 8) | low;
+
+    return true;
+}
+
+// DecodedOpcode
+static DecodedOpcode chip8_decode(uint16_t opcode) {
+    DecodedOpcode decoded = {
+        .x = (opcode & 0x0F00) >> 8,
+        .y = (opcode & 0x00F0) >> 4,
+        .n = opcode & 0x00F,
+        .nn = opcode & 0x00FF,
+        .nnn = opcode & 0x0FFF
+    };
+
+    return decoded;
+}
--- src/chip8.h
+++ src/chip8.h
@@ -32,6 +32,15 @@ typedef struct {
     bool halted;
 } Chip8;
 
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+    uint8_t n;
+    uint8_t nn;
+    uint8_t nnn;
+} DecodedOpcode;
+
+//  Chip8
 void chip8_init(Chip8 *chip8);
 
 bool chip8_load_program(Chip8 *chip8, const uint8_t *program, size_t size);
@@ -39,5 +48,8 @@ bool chip8_load_program(Chip8 *chip8, const uint8_t *program, size_t size);
 bool chip8_cycle(Chip8 *chip8);
 
 void chip8_dump_state(const Chip8 *chip8);
+
+// DecodedOpcode
+static DecodedOpcode chip8_decode(uint16_t opcode);
 
 #endif
